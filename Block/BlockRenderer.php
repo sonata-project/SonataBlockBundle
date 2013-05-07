a4c0af841b76bc7b98b49fa5a0aa2bde44209b1b
Thomas Rabaix <thomas.rabaix@sonata-project.org>
2013-05-07 14:03:25 +0200

    Fix useless code due to the BlockContextManager

--- Block/BlockRenderer.php
+++ Block/BlockRenderer.php
@@ -73,10 +73,6 @@ class BlockRenderer implements BlockRendererInterface
             $service->load($block);
 
             if (null === $response) {
-                // In order to have the block's response's isCacheable() to true
-                $defaultSettings = $service->getDefaultSettings();
-                $block->setSetting('ttl', isset($defaultSettings['ttl']) ? $defaultSettings['ttl'] : $block->getTtl());
-
                 $response = new Response();
                 $response->setTtl($block->getTtl());
             }
