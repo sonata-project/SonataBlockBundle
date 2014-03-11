b68103c67bc8ba34ca0cacc24657592c52be00bd
Hugo Briand <briand@ekino.com>
2014-03-11 14:44:12 +0100

    Remove unused code

--- Form/Type/ContainerTemplateType.php
+++ Form/Type/ContainerTemplateType.php
@@ -61,24 +61,7 @@ class ContainerTemplateType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'context'           => false,
-            'multiple'          => false,
-            'expanded'          => false,
             'choices'           => $this->templateChoices,
-            'preferred_choices' => array(),
-            'empty_data'        => function (Options $options) {
-                    $multiple = isset($options['multiple']) && $options['multiple'];
-                    $expanded = isset($options['expanded']) && $options['expanded'];
-
-                    return $multiple || $expanded ? array() : '';
-                },
-            'empty_value'       => function (Options $options, $previousValue) {
-                    $multiple = isset($options['multiple']) && $options['multiple'];
-                    $expanded = isset($options['expanded']) && $options['expanded'];
-
-                    return $multiple || $expanded || !isset($previousValue) ? null : '';
-                },
-            'error_bubbling'    => false,
         ));
     }
 }
