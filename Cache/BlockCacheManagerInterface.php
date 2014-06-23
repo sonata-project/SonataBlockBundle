118fdcadc901f1c10fcc52caf6b86b8d8246f2b2
Sergey Polischuk <spolischook@gmail.com>
2014-06-23 10:48:51 +0300

    Added BlockCacheManagerInterface and its implementation

---
+++ Cache/BlockCacheManagerInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sonata\BlockBundle\Cache;
+
+use Sonata\BlockBundle\Model\BlockInterface;
+
+interface BlockCacheManagerInterface
+{
+    /**
+     * @param BlockInterface $block
+     *
+     * @return boolean
+     */
+    public function setBlockToCache(BlockInterface $block);
+
+    /**
+     * @param  string          $blockName
+     * @param  array           $cacheKeys
+     * @return BlockInterface;
+     */
+    public function getBlockFromCache($blockName, array $cacheKeys);
+
+    /**
+     * @param BlockInterface $block
+     *
+     * @return \Sonata\Cache\CacheAdapterInterface;
+     */
+    public function getCacheService(BlockInterface $block);
+}
