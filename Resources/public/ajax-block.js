baf316f806d1d0ca9c5c3ac68094c1793b6a7b61
core23 <mail@core23.de>
2017-08-11 08:34:59 +0200

    Ajax Widget

---
+++ Resources/public/ajax-block.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+(function ($) {
+    function fetchBlock($block) {
+        var overlay = $('<div class="overlay"><div class="fa fa-refresh fa-spin"></div></div>');
+        // missing AdminLTE css for info-box necessary for overlay
+        $block.find('.overlay-wrapper').css('position', 'relative');
+        // show a loading indicator
+        $block.find('.overlay-wrapper').append(overlay);
+        return $.ajax($block.data('url')).done(function (json) {
+            // populate the block with html
+            $block.find('.url-count').html(json.count);
+            $block.find('.url-progress').css('width', json.progress + '%');
+            $block.find('.url-info').html(json.info);
+        }).fail(function () {
+            // show an error symbol
+            $block.find('.url-count').html('<div class="fa fa-remove text-danger"></div>');
+            $block.find('.url-progress').css('width', '0%');
+            $block.find('.url-info').html('');
+        }).always(function () {
+            // hide the loading indicator
+            $block.find(overlay).remove();
+        })
+    }
+
+    $(function () {
+        $('.sonata-ajax-block').each(function (i, block) {
+            var $block = $(block);
+            fetchBlock($block);
+        });
+        // for users to be able to refresh the box content on click
+        $('.sonata-ajax-block').on('click', '.refresh-btn', function (event) {
+            var $block = $(event.target).closest('.sonata-ajax-block');
+            fetchBlock($block)
+        })
+    })
+}(jQuery));
