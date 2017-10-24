978f0cf4c256b12f6930325922480ae3f3a25ad4
Sullivan SENECHAL <soullivaneuh@gmail.com>
2017-10-24 10:15:13 +0200

    Deprecate options resolver trick

--- Resources/stubs/symfony2.php
+++ Resources/stubs/symfony2.php
@@ -12,6 +12,9 @@
 namespace Symfony\Component\OptionsResolver;
 
 if (!interface_exists('Symfony\Component\OptionsResolver\OptionsResolverInterface')) {
+    /**
+     * @deprecated since 3.x, to be removed in 4.0. Use \Symfony\Component\OptionsResolver\OptionsResolver instead.
+     */
     interface OptionsResolverInterface
     {
     }
