442ee30c7a28c2a5b84f7285415dafec589a6ac8
ramol <ramol@ramol.pl>
2018-07-18 15:03:13 +0200

    Update BlockRenderer.php
    Hi,
    I hope I understand correctly author intentions but...
    Without this IF all children got TTL from first child.
    For example if you have footer container with 2 child:
    - sonata.block.service.text (default TTL: 86400, sonata.cache.noop) 
    - custom.sonata.media.block.gallery (TTL: 30, sonata.cache.memcached)
    addMetaInformation function overwrite custom.sonata.media.block.gallery TTL to 86400
    
    Best regards and thanks for your hard work with whole project.

--- src/Block/BlockRenderer.php
+++ src/Block/BlockRenderer.php
@@ -143,7 +143,9 @@ class BlockRenderer implements BlockRendererInterface
     {
         // a response exists, use it
         if ($this->lastResponse && $this->lastResponse->isCacheable()) {
-            $response->setTtl($this->lastResponse->getTtl());
+            if($this->lastResponse->getTtl()<$response->getTtl()) {
+                $response->setTtl($this->lastResponse->getTtl());
+            }
             $response->setPublic();
         } elseif ($this->lastResponse) { // not cacheable
             $response->setPrivate();
