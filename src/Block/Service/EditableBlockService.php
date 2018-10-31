aca85f77de1576ec923605d1bb063a0bc8fe141c
Christian Gripp <core23@users.noreply.github.com>
2018-10-31 09:13:53 +0100

    Decouple bundle from AdminBundle (#513)

---
+++ src/Block/Service/EditableBlockService.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\BlockBundle\Block\Service;
+
+use Sonata\BlockBundle\Form\Mapper\FormMapper;
+use Sonata\BlockBundle\Meta\MetadataInterface;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\Form\Validator\ErrorElement;
+
+/**
+ * @author Christian Gripp <mail@core23.de>
+ */
+interface EditableBlockService
+{
+    public function configureEditForm(FormMapper $form, BlockInterface $block);
+
+    public function configureCreateForm(FormMapper $form, BlockInterface $block);
+
+    public function validate(ErrorElement $errorElement, BlockInterface $block);
+
+    /**
+     * @param string|null $code
+     *
+     * @return MetadataInterface
+     */
+    public function getMetadata($code = null);
+}
