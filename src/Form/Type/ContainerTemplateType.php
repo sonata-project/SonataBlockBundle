2af8888ac38e00ecc1fde716467d915221ad00e4
KevinM2k <KevinM2k@gmail.com>
2018-12-08 17:32:35 +0000

    Change the ChoiceType Filter (#528)
    Causing an error when trying to modify an auto generated container block

--- src/Form/Type/ContainerTemplateType.php
+++ src/Form/Type/ContainerTemplateType.php
@@ -11,8 +11,8 @@
 
 namespace Sonata\BlockBundle\Form\Type;
 
-use Sonata\AdminBundle\Form\Type\Filter\ChoiceType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
