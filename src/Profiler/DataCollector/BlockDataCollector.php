6a4ab13baedb7d06b2bd40e3a67d5c4382b73214
Toni Rudolf <toni.rudolf@gmail.com>
2019-03-19 09:13:52 +0100

    Removed unused use declaration

--- src/Profiler/DataCollector/BlockDataCollector.php
+++ src/Profiler/DataCollector/BlockDataCollector.php
@@ -17,7 +17,6 @@ use Sonata\BlockBundle\Templating\Helper\BlockHelper;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
-use Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface;
 
 /**
  * Block data collector for the symfony web profiling.
