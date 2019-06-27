799c6887bf1767c6a70b18b18686772ee3916462
Peter Gribanov <wcode404@gmail.com>
2019-06-27 17:06:05 +0300

    add test debug blocks

--- tests/Command/DebugBlocksCommandTest.php
+++ tests/Command/DebugBlocksCommandTest.php
@@ -15,9 +15,11 @@ namespace Sonata\BlockBundle\Tests\Command;
 
 use PHPUnit\Framework\TestCase;
 use Sonata\BlockBundle\Block\BlockServiceManagerInterface;
+use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Sonata\BlockBundle\Command\DebugBlocksCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * @author Javier Spagnoletti <phansys@gmail.com>
@@ -92,5 +94,68 @@ class DebugBlocksCommandTest extends TestCase
         $this->expectExceptionMessage('Argument 2 passed to Sonata\BlockBundle\Command\DebugBlocksCommand::__construct() must be an instance of Sonata\BlockBundle\Block\BlockServiceManagerInterface, NULL given.');
 
         new DebugBlocksCommand();
+    }
+
+    public function testDebugBlocks(): void
+    {
+        $this->application = new Application();
+
+        $blockManager = $this->createMock(BlockServiceManagerInterface::class);
+        $blockManager
+            ->expects($this->any())
+            ->method('getServices')
+            ->willReturn([
+                'test.without_options' => new class extends AbstractBlockService {
+                    public function getName()
+                    {
+                        return 'Test service block without options';
+                    }
+                },
+                'test.with_simple_option' => new class extends AbstractBlockService {
+                    public function configureSettings(OptionsResolver $resolver)
+                    {
+                        $resolver->setDefault('limit', 150);
+                        $resolver->setAllowedTypes('limit', 'int');
+                    }
+
+                    public function getName()
+                    {
+                        return 'Test service block with simple option';
+                    }
+                },
+                'test.with_required_option' => new class extends AbstractBlockService {
+                    public function configureSettings(OptionsResolver $resolver)
+                    {
+                        $resolver->setRequired('limit');
+                        $resolver->setAllowedTypes('limit', 'int');
+                    }
+
+                    public function getName()
+                    {
+                        return 'Test service block with required option';
+                    }
+                },
+            ]);
+
+        $this->application->add(new DebugBlocksCommand(null, $blockManager));
+
+        $command = $this->application->find('debug:sonata:block');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => 'debug:sonata:block']);
+
+        $expected = <<<EOF
+
+>> Test service block without options (test.without_options)
+
+>> Test service block with simple option (test.with_simple_option)
+    limit                         150
+
+>> Test service block with required option (test.with_required_option)
+    limit
+done!
+
+EOF;
+
+        $this->assertSame($expected, $commandTester->getDisplay());
     }
 }
