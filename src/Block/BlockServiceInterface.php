b12633e2262a8dc79079bae7132c430996bb3d33
core23 <mail@core23.de>
2019-07-04 20:13:15 +0200

    Updated deprecation message

--- src/Block/BlockServiceInterface.php
+++ src/Block/BlockServiceInterface.php
@@ -34,7 +34,7 @@ interface BlockServiceInterface
     public function execute(BlockContextInterface $blockContext, Response $response = null);
 
     /**
-     * @deprecated since 3.x, to be removed in 4.0
+     * @deprecated since sonata-project/block-bundle 3.x, to be removed in 4.0
      *
      * @return string
      */
