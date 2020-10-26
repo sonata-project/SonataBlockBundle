6ff2640ae793bf25d6ad00fefe3f2ab644923054
Jordi <jordism91@gmail.com>
2020-10-26 20:34:19 +0100

    Fix php8 build
    On php8 anonymous classes extending others are formed
    with: parent_class@anonymous so its better to simply check for
    @anonymous instead of relying on the initial part of the string

--- tests/Block/Service/CustomBlockServiceTest.php
+++ tests/Block/Service/CustomBlockServiceTest.php
@@ -15,21 +15,24 @@ namespace Sonata\BlockBundle\Tests\Block\Service;
 
 use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Sonata\BlockBundle\Test\BlockServiceTestCase;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 /**
  * @author Javier Spagnoletti <phansys@gmail.com>
  */
 final class CustomBlockServiceTest extends BlockServiceTestCase
 {
+    use ExpectDeprecationTrait;
+
     /**
      * NEXT_MAJOR: Remove this test.
      *
      * @group legacy
-     *
-     * @expectedDeprecation Passing string as argument 1 to Sonata\BlockBundle\Block\Service\AbstractBlockService@anonymous::__construct() is deprecated since sonata-project/block-bundle 3.16 and will throw a \TypeError as of 4.0. You must pass an instance of Twig\Environment instead.
      */
     public function testArgumentDeprecation()
     {
+        $this->expectDeprecation('Passing string as argument 1 to Sonata\BlockBundle\Block\Service\AbstractBlockService@anonymous::__construct() is deprecated since sonata-project/block-bundle 3.16 and will throw a \TypeError as of 4.0. You must pass an instance of Twig\Environment instead.');
+
         new class('block') extends AbstractBlockService {
         };
     }
