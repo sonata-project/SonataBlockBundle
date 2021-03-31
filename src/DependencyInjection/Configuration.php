2e1562e84cf63726392f51eff79d4316b54f8562
Sullivan SENECHAL <soullivaneuh@gmail.com>
2021-03-31 09:43:36 +0200

    Applied fixes from FlintCI

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -231,8 +231,7 @@ class Configuration implements ConfigurationInterface
                         ->end()
                     ->end()
                 ->end()
-            ->end()
-        ;
+            ->end();
 
         return $treeBuilder;
     }
