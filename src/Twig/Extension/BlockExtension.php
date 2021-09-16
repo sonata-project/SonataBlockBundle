77de98671432341f5fb4a76e093ca89bd8bc398b
Jordi Sala Morales <jordism91@gmail.com>
2021-09-16 08:56:08 +0200

    Fix deprecation on Symfony 5.4

--- src/Twig/Extension/BlockExtension.php
+++ src/Twig/Extension/BlockExtension.php
@@ -19,7 +19,7 @@ use Twig\TwigFunction;
 
 final class BlockExtension extends AbstractExtension
 {
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction(
