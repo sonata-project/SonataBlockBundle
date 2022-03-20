1f82fce35af724ec73cd64311252af62c42b061c
Jordi Sala Morales <jordism91@gmail.com>
2022-03-20 10:22:57 +0100

    Fix cs fixer

--- src/Resources/config/block.php
+++ src/Resources/config/block.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Sonata\BlockBundle\Block\Service\ContainerBlockService;
 use Sonata\BlockBundle\Block\Service\EmptyBlockService;
 use Sonata\BlockBundle\Block\Service\RssBlockService;
--- src/Resources/config/exception.php
+++ src/Resources/config/exception.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Sonata\BlockBundle\Exception\BlockExceptionInterface;
 use Sonata\BlockBundle\Exception\Filter\DebugOnlyFilter;
 use Sonata\BlockBundle\Exception\Filter\IgnoreClassFilter;
