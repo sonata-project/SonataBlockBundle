1bebc41a3fc98e9808062fda7bb226d964f5f45c
David Maicher <mail@dmaicher.de>
2022-04-23 19:12:14 +0200

    fix BlockHelper service definition

--- src/Resources/config/core.php
+++ src/Resources/config/core.php
@@ -70,8 +70,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             new ReferenceConfigurator('sonata.block.context_manager'),
             new ReferenceConfigurator('event_dispatcher'),
             (new ReferenceConfigurator('debug.stopwatch'))->nullOnInvalid(),
-            (new ReferenceConfigurator('sonata.cache.manager'))->nullOnInvalid(),
-            (new ReferenceConfigurator('sonata.block.cache.handler'))->nullOnInvalid(),
         ]);
 
     $services->set('sonata.block.loader.chain', BlockLoaderChain::class)
