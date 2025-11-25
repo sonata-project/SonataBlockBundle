1975533a90683a3844fc79e3f0bd119ef55cafa1
Erison Silva <erison.sdn@gmail.com>
2025-11-25 14:16:49 +0100

    Add default_protocol for urlType

--- src/Block/Service/RssBlockService.php
+++ src/Block/Service/RssBlockService.php
@@ -56,6 +56,7 @@ final class RssBlockService extends AbstractBlockService implements EditableBloc
             'keys' => [
                 ['url', UrlType::class, [
                     'required' => false,
+                    'default_protocol' => 'https',
                     'label' => 'form.label_url',
                     'translation_domain' => 'SonataBlockBundle',
                 ]],
